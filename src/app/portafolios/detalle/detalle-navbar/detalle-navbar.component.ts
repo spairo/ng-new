e3819ed9e306c268a6f87644647582bfea37500e
spairo <nelixhost@gmail.com>
2018-04-11 19:07:44 -0500

    feat(detalle)navbar component added

--- src/app/portafolios/detalle/detalle-navbar/detalle-navbar.component.ts
+++ src/app/portafolios/detalle/detalle-navbar/detalle-navbar.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'new-detalle-navbar',
   templateUrl: './detalle-navbar.component.html',
   styleUrls: ['./detalle-navbar.component.styl']
 })
+
 export class DetalleNavbarComponent implements OnInit {
 
-  constructor() { }
+  public distance: number;
+
+  constructor(private element: ElementRef) {}
 
   ngOnInit() {
+    this.distance = this.element.nativeElement.offsetTop;
   }
 
+  @HostListener('window:scroll', [])
+    onWindowScroll() {
+      if (window.scrollY >= this.distance) {
+        this.element.nativeElement.className = 'fixedBar';
+      } else {
+        this.element.nativeElement.classList.remove('fixedBar');
+      }
+    }
 }
