5c74ba2d65fdadb2b83d68b552c640333be236e8
spairo <nelixhost@gmail.com>
2018-05-09 17:14:18 -0500

    fix(dropdown): css and performance

--- src/app/shared/dropdown/dropdown.component.ts
+++ src/app/shared/dropdown/dropdown.component.ts
@@ -2,32 +2,25 @@ import { Component, OnInit, Input, Output, OnChanges, EventEmitter } from '@angu
 
 @Component({
   selector: 'new-dropdown',
-  //template: ``,
   templateUrl: './dropdown.component.html',
   styleUrls: ['./dropdown.component.styl']
 })
 export class DropdownComponent implements OnInit {
 
-  public opened: boolean = true;
-  public foo: string;
-  public disabled: any;
-
   @Input() options: any;
   @Input() label: string;
-  @Output() public onUpdate: EventEmitter<any> = new EventEmitter();
 
-  constructor() { }
+  public opened: boolean = true;
+  //public disabled: any;
+  public counter: any;
+  public picked: string;
+
+  constructor() {}
 
   ngOnInit() {}
 
-  runOnComplete(): void {
-    console.log(this.onUpdate);
-    this.onUpdate.emit();
+  selected(contract){
+    this.picked = contract;
   }
-
-
-  on_complete(){}
-
-  onOpen(){}
 
 }
